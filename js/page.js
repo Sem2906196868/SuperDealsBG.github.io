4a696b9bb2431a15208adc8c2f03c687b2c63d10
TGViewer <TGViewer.App@gmail.com>
2018-08-07 14:23:52 +0300

    Load data

--- js/page.js
+++ js/page.js
@@ -1,3 +1,11 @@
-﻿function init() {
-  return true;
-}
+﻿function lazyLoad() {
+	var images = document.querySelectorAll('.lazy_load');
+	for (var i = 0; i < images.length; i++) {
+		var image = images[i];
+		var dataSrc = image.getAttribute('data-src');
+		if (dataSrc != '') {
+			image.src = dataSrc;
+			image.setAttribute('data-src', '');
+		}
+	}	
+};
