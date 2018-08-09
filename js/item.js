cd6724a8d9dc04f3634373a7da2e5dc861a3a1ab
TGViewer <TGViewer.App@gmail.com>
2018-08-09 14:41:53 +0300

    Load data

--- js/item.js
+++ js/item.js
@@ -1,3 +1,13 @@
-﻿function init() {
-  return true;
+﻿function getOriginalImage() {
+	var imgs = document.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++) {
+		var currentImage = imgs[i].src;
+		if (currentImage.toLowerCase().indexOf("alicdn.com".toLowerCase()) >= 0) {
+			currentImage.replace(".jpg_350x350.jpg", ".jpg").replace(".jpg_220x220.jpg", ".jpg");
+			imgs[i].src = currentImage;
+			break;
+		}
+	}	
 }
+
+getOriginalImage();
