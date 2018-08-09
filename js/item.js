32ab4e62cfd9c429387a5e2adcfe2eda6f47a057
TGViewer <TGViewer.App@gmail.com>
2018-08-09 15:48:56 +0300

    Load data

--- js/item.js
+++ js/item.js
@@ -3,7 +3,7 @@
 	for (var i = 0; i < imgs.length; i++) {
 		var currentImage = imgs[i].src;
 		if (currentImage.toLowerCase().indexOf("alicdn.com".toLowerCase()) >= 0) {
-			currentImage.replace(".jpg_350x350.jpg", ".jpg").replace(".jpg_220x220.jpg", ".jpg");
+			currentImage = currentImage.replace(".jpg_350x350.jpg", ".jpg").replace(".jpg_220x220.jpg", ".jpg");
 			imgs[i].src = currentImage;
 			break;
 		}
